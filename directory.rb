14d0fbe77009360151565b6def26db000cda65ac
sanjsanj <sanjsanj@hotmail.com>
2015-03-09 10:22:39 +0000

    Make directory interactive, allow student input from terminal

--- directory.rb
+++ directory.rb
@@ -28,6 +28,26 @@ def print_footer(names)
   puts "Overall, we have #{names.length} great students"
 end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit enter twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat the code
+  while !name.empty? do
+    # add the students hash to the array
+    students << {:name => name, :cohort => :march}
+    puts "Now we have #{students.length} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
