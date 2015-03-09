7cdbe4e9909ba761a283310ff12f92eb0c2a9ac8
sanjsanj <sanjsanj@hotmail.com>
2015-03-09 10:57:19 +0000

    Complete exercises 1, 2, 3

--- directory.rb
+++ directory.rb
@@ -14,23 +14,29 @@ students = [
   ]
 
 def print_header
-  puts "The students of March 2015 cohort at Makers Academy"
-  puts "--------------"
+  print "The students of March 2015 cohort at Makers Academy\n"
+  print "--------------\n"
 end
 
-def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+def print_students(students)
+  students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_students_with_a(students)
+  students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name][0].downcase == "a"
   end
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.length} great students"
+  print "Overall, we have #{names.length} great students\n"
 end
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit enter twice"
+  print "Please enter the names of the students\n"
+  print "To finish, just hit enter twice\n"
   # create an empty array
   students = []
   # get the first name
@@ -39,7 +45,7 @@ def input_students
   while !name.empty? do
     # add the students hash to the array
     students << {:name => name, :cohort => :march}
-    puts "Now we have #{students.length} students"
+    print "Now we have #{students.length} students\n"
     # get another name from the user
     name = gets.chomp
   end
@@ -49,5 +55,6 @@ end
 
 students = input_students
 print_header
-print(students)
+# print_students(students)
+print_students_with_a(students)
 print_footer(students)
