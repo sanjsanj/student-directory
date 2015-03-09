775c55c5aa24ae8b6ff980476efa0e487c939df7
sanjsanj <sanjsanj@hotmail.com>
2015-03-09 16:46:35 +0000

    Complete ex 10

--- directory.rb
+++ directory.rb
@@ -26,18 +26,20 @@ def print_students_march(students)
 end
 
 def print_footer(names)
-  print "Overall, we have #{names.length} great students\n"
+  names.length == 1 ? pluralize = "student" : pluralize = "students"
+  print "Overall, we have #{names.length} great #{pluralize}\n"
 end
 
 def input_student_data
   print "Please enter the names of the student, their cohort, their hobby and country of birth seperated by ',' without any spaces\n"
   print "To finish, just hit enter twice\n"
-  students = Array.new
+  students = []
   name = gets.chomp
   while !name.empty? do
     studary = name.split(",")
     students << {:name => studary[0], :cohort => studary[1].to_sym, :hobby => studary[2], :cob => studary[3]}
-    print "Now we have #{students.length} students\n"
+    students.length == 1 ? pluralize = "student" : pluralize = "students"
+    print "Now we have #{students.length} #{pluralize}\n"
     name = gets.chomp
   end
   students
