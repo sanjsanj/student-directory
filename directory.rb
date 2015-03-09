d9880c55da9e16f7a16c3a3e232a818cd5988ec1
sanjsanj <sanjsanj@hotmail.com>
2015-03-09 20:05:34 +0000

    Add file save option, add .gitignore

--- directory.rb
+++ directory.rb
@@ -39,16 +39,18 @@ end
 # print_students_march(students)
 # print_footer(students)
 
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
-end
-  
-def show_students
-  print_header
-  print_students_list
-  print_footer
 end
   
 def process(selection)
@@ -57,6 +59,8 @@ def process(selection)
       input_student_data
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -64,11 +68,21 @@ def process(selection)
   end
 end
 
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
-  end
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort], student[:hobby], student[:cob]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+  puts "File saved"
+end
+  
 interactive_menu
