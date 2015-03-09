9b519c6a7da09b074dbf592821f85ddd9d07e323
sanjsanj <sanjsanj@hotmail.com>
2015-03-09 09:56:43 +0000

    Create array for students and iterate over them

--- directory.rb
+++ directory.rb
@@ -1,19 +1,25 @@
-student_count = 11
+# creating an array of students
+students = [
+  "Andrea Armiliato",
+  "Caron Stace",
+  "Maciej Kurek",
+  "Paul Wallis",
+  "Guillaume Bouffard",
+  "George McGowan",
+  "Sarah O'Connor",
+  "Richard Lynch",
+  "Max Weaver",
+  "Phoebe Hugh",
+  "Sanjay Purswani"
+  ]
 
-# first we print the list of students
+# then print the list of students
 puts "The students of March 2015 cohort at Makers Academy"
 puts "--------------"
-puts "Andrea Armiliato"
-puts "Caron Stace"
-puts "Maciej Kurek"
-puts "Paul Wallis"
-puts "Guillaume Bouffard"
-puts "George McGowan"
-puts "Sarah O'Connor"
-puts "Richard Lynch"
-puts "Max Weaver"
-puts "Phoebe Hugh"
-puts "Sanjay Purswani"
 
-# then we print the student count
-puts "Overall, we have #{student_count} great students"
+students.each do |student|
+  puts student
+end
+
+# then print the student count
+puts "Overall, we have #{students.length} great students"
